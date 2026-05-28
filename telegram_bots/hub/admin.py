2b80d6f2bfbeb4beabdf4322ba5fc6b8f687fad8
exg1o <77574092+EXG1O@users.noreply.github.com>
2026-05-29 02:39:41 +0300

    Update `TelegramBotsHub` model of `telegram_bots/hub` app (#294)

--- telegram_bots/hub/admin.py
+++ telegram_bots/hub/admin.py
@@ -5,6 +5,6 @@ from .models import TelegramBotsHub
 
 @admin.register(TelegramBotsHub)
 class TelegramBotsHubAdmin(admin.ModelAdmin[TelegramBotsHub]):
-    search_fields = ['url']
-    list_display = ['url']
-    fields = ['url', 'service_token', 'microservice_token']
+    search_fields = ['id', 'url']
+    list_display = ['id', 'url']
+    fields = ['id', 'url', 'telegram_token', 'service_token', 'microservice_token']
