db4739c8eb0a7a42965f4148e0b03927f3c5fe79
exg1o <77574092+EXG1O@users.noreply.github.com>
2026-05-29 03:32:52 +0300

    Add infrastructure for handling and forwarding Telegram webhooks to microservice in `telegram_bots/hub` app (#296)

--- webhooks/tests.py
+++ webhooks/tests.py
@@ -4,15 +4,46 @@ from django.urls import reverse
 from rest_framework import status
 from rest_framework.request import Request
 from rest_framework.response import Response
-from rest_framework.test import APIRequestFactory
+from rest_framework.test import APIRequestFactory, force_authenticate
 
+from constructor_telegram_bots.utils.tests import assert_view_basic_protected
+from telegram_bots.hub.tests.mixins import HubMixin
 from telegram_bots.models.trigger import Trigger, TriggerWebhook
 from telegram_bots.tests.mixins import TelegramBotMixin
 from users.tests.mixins import UserMixin
 
-from .views import TriggerWebhookAPIView
+from .views import TelegramAPIView, TriggerWebhookAPIView
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, Any
+from unittest.mock import MagicMock, patch
+import json
+
+
+class TelegramAPIViewTests(TelegramBotMixin, UserMixin, HubMixin, TestCase):
+    def setUp(self) -> None:
+        super().setUp()
+        self.factory = APIRequestFactory()
+
+    @patch('telegram_bots.hub.models.TelegramBotsHub.client')
+    def test_post(self, mock_client: MagicMock) -> None:
+        view = TelegramAPIView.as_view()
+        data: dict[str, Any] = {'key': 'value'}
+
+        request: Request = self.factory.post(
+            reverse('api:webhooks:telegram', kwargs={'bot_id': self.telegram_bot.id}),
+            data=data,
+            content_type='application/json',
+        )
+        assert_view_basic_protected(
+            view, request, self.hub.telegram_token, bot_id=self.telegram_bot.id
+        )
+        force_authenticate(request, self.hub, self.hub.telegram_token)  # type: ignore [arg-type]
+
+        response: Response = view(request, bot_id=self.telegram_bot.id)
+        self.assertEqual(response.status_code, status.HTTP_202_ACCEPTED)
+        mock_client.forward_telegram_data.assert_called_once_with(
+            bot_id=self.telegram_bot.id, data=json.dumps(data).encode()
+        )
 
 
 class TriggerWebhookAPIViewTests(TelegramBotMixin, UserMixin, TestCase):
