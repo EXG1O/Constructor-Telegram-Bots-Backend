d60ab931c33261e24eec95b4b56130bd22610e41
exg1o <77574092+EXG1O@users.noreply.github.com>
2026-06-11 09:45:29 +0300

    Create `BackgroundTaskFilter` filter for `BackgroundTaskViewSet` view in `telegram_bots/hub` app (#301)

--- telegram_bots/hub/views/background_task.py
+++ telegram_bots/hub/views/background_task.py
@@ -3,6 +3,8 @@ from django.db.models import QuerySet
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
+from django_filters.rest_framework import BooleanFilter, DjangoFilterBackend, FilterSet
+
 from constructor_telegram_bots.mixins import IDLookupMixin
 
 from ...models import BackgroundTask
@@ -11,12 +13,29 @@ from ..serializers import BackgroundTaskSerializer
 from .mixins import TelegramBotMixin
 
 
+class BackgroundTaskFilter(FilterSet):
+    has_source_connections = BooleanFilter(
+        field_name='source_connections', method='filter_has_field'
+    )
+
+    def filter_has_field(
+        self, queryset: QuerySet[BackgroundTask], name: str, value: bool
+    ) -> QuerySet[BackgroundTask]:
+        return queryset.filter(**{f'{name}__isnull': not value})
+
+    class Meta:
+        model = BackgroundTask
+        fields = ['has_source_connections']
+
+
 class BackgroundTaskViewSet(
     IDLookupMixin, TelegramBotMixin, ReadOnlyModelViewSet[BackgroundTask]
 ):
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
     serializer_class = BackgroundTaskSerializer
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = BackgroundTaskFilter
 
     def get_queryset(self) -> QuerySet[BackgroundTask]:
         background_tasks: QuerySet[BackgroundTask] = (
