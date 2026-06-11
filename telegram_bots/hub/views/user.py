6f3aa898c31cd2a716c1195b638cd5ec472f8307
exg1o <77574092+EXG1O@users.noreply.github.com>
2026-06-11 08:54:47 +0300

    Add `LimitOffsetPagination` pagination to `UserViewSet` view in `telegram_bots/hub` app (#300)

--- telegram_bots/hub/views/user.py
+++ telegram_bots/hub/views/user.py
@@ -5,6 +5,7 @@ from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
 from constructor_telegram_bots.mixins import IDLookupMixin
+from constructor_telegram_bots.pagination import LimitOffsetPagination
 
 from ...models import User
 from ..authentication import TokenAuthentication
@@ -21,6 +22,7 @@ class UserViewSet(
     authentication_classes = [TokenAuthentication]
     permission_classes = [IsAuthenticated]
     serializer_class = UserSerializer
+    pagination_class = LimitOffsetPagination
 
     def get_queryset(self) -> QuerySet[User]:
         return self.telegram_bot.users.all()
