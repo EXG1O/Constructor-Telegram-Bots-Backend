e9edb33efcffab8e63c658040ac3ea23e725848b
exg1o <77574092+EXG1O@users.noreply.github.com>
2026-06-19 12:08:09 +0300

    Create `ChatViewSet` and `UserViewSet` view filters in `telegram_bots/hub` app (#314)

--- telegram_bots/hub/views/user.py
+++ telegram_bots/hub/views/user.py
@@ -4,6 +4,9 @@ from rest_framework.mixins import CreateModelMixin
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.viewsets import ReadOnlyModelViewSet
 
+from django_filters.rest_framework import DjangoFilterBackend, FilterSet
+
+from constructor_telegram_bots.filters import NumberInFilter
 from constructor_telegram_bots.mixins import IDLookupMixin
 from constructor_telegram_bots.pagination import LimitOffsetPagination
 
@@ -11,6 +14,15 @@ from ...models import User
 from ..authentication import TokenAuthentication
 from ..serializers import UserSerializer
 from .mixins import TelegramBotMixin
+
+
+class UserFilter(FilterSet):
+    ids = NumberInFilter(field_name='id', lookup_expr='in')
+    telegram_ids = NumberInFilter(field_name='telegram_id', lookup_expr='in')
+
+    class Meta:
+        model = User
+        fields = ['ids', 'telegram_ids']
 
 
 class UserViewSet(
@@ -23,6 +35,8 @@ class UserViewSet(
     permission_classes = [IsAuthenticated]
     serializer_class = UserSerializer
     pagination_class = LimitOffsetPagination
+    filter_backends = [DjangoFilterBackend]
+    filterset_class = UserFilter
 
     def get_queryset(self) -> QuerySet[User]:
         return self.telegram_bot.users.all()
