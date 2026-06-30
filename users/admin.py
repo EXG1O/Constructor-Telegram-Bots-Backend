955682c6f111fc6537323195a98fd389034f8c00
exg1o <77574092+EXG1O@users.noreply.github.com>
2026-06-30 17:05:06 +0300

    Fix media cleanup on user deletion in `users` app (#324)

--- users/admin.py
+++ users/admin.py
@@ -1,10 +1,12 @@
 from django.contrib import admin
+from django.core.files.storage import default_storage
 from django.db.models import Count
 from django.db.models.query import QuerySet
 from django.http import HttpRequest
 from django.utils.translation import gettext_lazy as _
 
 from .models import User
+from .utils.storage import get_user_file_names
 
 
 @admin.register(User)
@@ -61,6 +63,25 @@ class UserAdmin(admin.ModelAdmin[User]):
             .get_queryset(request)
             .annotate(telegram_bot_count=Count('telegram_bots'))
         )
+
+    def delete_model(self, request: HttpRequest, user: User) -> None:
+        file_names: set[str] = get_user_file_names(user)
+
+        super().delete_model(request, user)
+
+        for file_name in file_names:
+            default_storage.delete(file_name)
+
+    def delete_queryset(self, request: HttpRequest, queryset: QuerySet[User]) -> None:
+        file_names: set[str] = set()
+
+        for user in queryset:
+            file_names.update(get_user_file_names(user))
+
+        super().delete_queryset(request, queryset)
+
+        for file_name in file_names:
+            default_storage.delete(file_name)
 
     @admin.display(description=_('Telegram ботов'), ordering='telegram_bot_count')
     def telegram_bot_count(self, user: User) -> int:
