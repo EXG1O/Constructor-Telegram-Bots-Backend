6b2c9bc5944745e2313c9e85afa11e0ef961d995
exg1o <exg1o.me@gmail.com>
2026-08-01 15:58:19 +0300

    Fix `SubscriptionInvoiceAdmin` admin in `premium` app

--- premium/admin.py
+++ premium/admin.py
@@ -83,13 +83,7 @@ class SubscriptionInvoiceAdmin(admin.ModelAdmin[SubscriptionInvoice]):
         'paid_date',
         'updated_date',
     ]
-    actions = [
-        'make_pending',
-        'make_paid',
-        'make_failed',
-        'make_expired',
-        'make_refunded',
-    ]
+    actions = ['make_pending', 'make_paid', 'make_refunded']
 
     def get_queryset(self, request: HttpRequest) -> QuerySet[SubscriptionInvoice]:
         return super().get_queryset(request).select_related('user')
